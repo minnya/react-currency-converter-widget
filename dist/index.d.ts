ef520b8852784567774eb05cfa9bac2d692903a7
minnya <minnya9018@hotmail.co.jp>
2024-10-10 23:04:31 -0400

    Modified docs

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,7 @@
 import React from "react";
 interface CurrencyConverterProps {
+    defaultFromCurrency: string;
+    defaultToCurrency: string;
 }
 export declare const CurrencyConverter: React.FC<CurrencyConverterProps>;
 export {};
